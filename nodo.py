acf308ef96b3cc766e5c392b27ff10e7c7f3500c
PabloTamurayaUSAC <3016220860101@ingenieria.usac.edu.gt>
2023-08-10 09:59:56 -0600

    aaa

--- nodo.py
+++
@@ -1,9 +0,0 @@
-class Nodo:
-    def __init__(self):
-        self.indice = 0
-
-    def getIndice(self):
-        return self.indice
-
-    def setIndice(self, indice):
-        self.indice = indice
